cdbee946d40eb5f5841b24283deb4b2ff707b871
KWalkerNNK <kwalkernnk@gmail.com>
2022-09-29 10:12:34 +0700

    🤎🤍

--- src/auth/dto/dto.user.ts
+++ src/auth/dto/dto.user.ts
@@ -1,20 +1,25 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
 
 export class UserDto {
   id: number;
 
+  @IsNotEmpty()
   @IsString()
   fullName: string;
 
+  @IsNotEmpty()
   @IsString()
   phoneNumber: string;
 
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
   address: string;
 
+  @IsNotEmpty()
   @IsString()
   password: string;
 
